8235f863c729224c5c7edf7c3ac8ed8d3bab5a00
Hua Feng <hua.feng@99cloud.net>
2020-10-15 00:10:40 +0800

    Update error content.

--- request.go
+++ request.go
@@ -11,22 +11,22 @@ func (c *Client) Request(method, url string, param map[string]interface{}) error
 	// sign
 	sign, err := c.doSign(method, url, param)
 	if err != nil {
-		return fmt.Errorf("Sign failed, err: %s", err.Error())
+		return fmt.Errorf("Sign failed, error: %s", err.Error())
 	}
 
 	bytes, err := json.Marshal(sign)
 	if err != nil {
-		return fmt.Errorf("JSON marshal failed, err: %s", err.Error())
+		return fmt.Errorf("JSON marshal failed, error: %s", err.Error())
 	}
 
 	_, body, errs := gorequest.New().Post(fmt.Sprintf("%s?sign=%s", url, sign)).Send(string(bytes)).End()
 	if len(errs) > 0 {
-		return fmt.Errorf("HTTP request failed, err: %s", errs[0])
+		return fmt.Errorf("HTTP request failed, error: %s", errs[0])
 	}
 	res := &ResponseMessage{}
 	err = json.Unmarshal([]byte(body), res)
 	if err != nil {
-		return fmt.Errorf("JSON unmarshal failed, err: %s", err.Error())
+		return fmt.Errorf("JSON unmarshal failed, error: %s", err.Error())
 	}
 	if res.Ret != "SUCCESS" {
 		return fmt.Errorf("Umeng push failed, error message: %s, error code: %d",
